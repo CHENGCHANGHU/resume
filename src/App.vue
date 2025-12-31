3213573e548ef9b939f9d69cc1ef7d97bd714156
Changhu Cheng <1669675499@qq.com>
2025-12-31 02:42:16 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <img class="resume-image" src="https://resourcemanager.netlify.app/api/storage/view?id=659c066195180e1767bf" />
         <div>
           <h1>程昌虎</h1>
-          <div>前端开发工程师</div>
+          <div>安徽工业经济职业技术学院计算机与人工智能学院教师<br>兼任安徽省地质矿产勘查局数智专班组成员</div>
         </div>
       </div>
       <CollapsibleBox title="个人信息">
@@ -16,7 +16,7 @@
             <dt>出生年月</dt>
             <dd>1997年1月</dd>
             <dt>联系方式</dt>
-            <dd>13912970637</dd>
+            <dd>13912970637（同微信）</dd>
             <dt>邮箱</dt>
             <dd>1669675499@qq.com</dd>
             <dt>毕业院校</dt>
@@ -49,12 +49,16 @@
           <div class="work-container">
             <span>2020.07</span>
             <span>北京美团</span>
-            <span>前端技术中心（前端实习）</span>
+            <span>前端技术中心（前端实习工程师）</span>
             <span>就职于架构产品开发组，参与故障演练平台和服务治理平台的建设，主要使用Vue2进行开发</span>
-            <span>2021.07-至今</span>
+            <span>2021.07-2024.07</span>
             <span>深圳虾皮信息科技有限公司</span>
-            <span>卖家部门（前端开发）</span>
+            <span>卖家部门（前端开发工程师）</span>
             <span>先后就职于Seller Open Platform、Seller Onboarding团队，参与开发者开放平台、卖家增值服务、卖家入驻服务、卖家入驻低代码平台的建设，主要使用Vue2、Vue3、React Native进行开发</span>
+            <span>2024.08-至今</span>
+            <span>安徽工业经济职业技术学院</span>
+            <span>教师（计算机专业）</span>
+            <span>计算机与人工智能学院教师，兼任安徽省地质矿产勘查局数智专班组成员<br><b>教授课程：</b>网络操作系统及应用、C语言程序设计</span>
           </div>
         </template>
       </CollapsibleBox>
@@ -73,6 +77,18 @@
             <span>于东南大学获“五四表彰”优秀团员</span>
             <span>2022年6月</span>
             <span>于深圳虾皮信息科技有限公司获最佳进步奖</span>
+            <span>2025年2月</span>
+            <span>获安徽省职业技能竞赛-全省数字金融行业职业技能竞赛电子数据取证分析师赛项二等奖</span>
+            <span>2025年4月</span>
+            <span>指导学生获2024-2025年度安徽省职业院校技能大赛（高职组）“Web技术”赛项三等奖</span>
+            <span>2025年8月</span>
+            <span>获第三届全国工业互联网创新大赛职教赛道工业智能产线的协议智通与虚实互联赛题二等奖</span>
+            <span>2025年9月</span>
+            <span>指导学生作品《灵图启航-智慧文旅新图景》获安徽省大学生创新大赛（2025）职教赛道铜奖</span>
+            <span>2025年11月</span>
+            <span>获2025年安徽省信息化技术技能竞赛计算机程序设计员赛项二等奖</span>
+            <span>2025年12月</span>
+            <span>获安徽省职业技能竞赛—2025年全省通信和互联网行业职业技能竞赛暨2025-2026年全国行业职业技能竞赛—第四届全国电子企业职业技能竞赛计算机及外部设备装配调试员安徽选拔赛三等奖</span>
           </div>
         </template>
       </CollapsibleBox>
@@ -165,7 +181,9 @@ const codingStyleHTML = createHTMLString(transform(codingStyle, { output: 'optio
     "a b c"
     "d d d"
     "e f g"
-    "h h h";
+    "h h h"
+    "i j k"
+    "l l l";
   row-gap: 4px;
   
   > span:nth-child(1) { grid-area: a; }
@@ -176,19 +194,36 @@ const codingStyleHTML = createHTMLString(transform(codingStyle, { output: 'optio
   > span:nth-child(6) { grid-area: f; }
   > span:nth-child(7) { grid-area: g; }
   > span:nth-child(8) { grid-area: h; }
+  > span:nth-child(9) { grid-area: i; }
+  > span:nth-child(10) { grid-area: j; }
+  > span:nth-child(11) { grid-area: k; }
+  > span:nth-child(12) { grid-area: l; }
 
-  > span:nth-child(2), > span:nth-child(6) {
+  > span:nth-child(4n+2) {
     justify-self: center;
   }
 
-  > span:nth-child(3), > span:nth-child(7) {
+  // > span:nth-child(2), > span:nth-child(6) {
+  //   justify-self: center;
+  // }
+
+  > span:nth-child(4n+3) {
     justify-self: end;
   }
 
-  > span:nth-child(4), > span:nth-child(8) {
+  // > span:nth-child(3), > span:nth-child(7) {
+  //   justify-self: end;
+  // }
+
+  > span:nth-child(4n) {
     color: #999;
     font-size: 14px;
   }
+
+  // > span:nth-child(4), > span:nth-child(8) {
+  //   color: #999;
+  //   font-size: 14px;
+  // }
 
   > span:nth-child(4) {
     margin-bottom: 8px;
